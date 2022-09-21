fdd847e7bdbbaf31eaae992983b70397186e4457
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <phtfao@gmail.com>
2022-09-21 09:36:18 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!
 
 cp .env.example .env
-composer config --global --auth github-oauth.github.com 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
+composer config --global --auth github-oauth.github.com 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
 composer install
 chmod 777 storage -R
 cp -R vendor/phtfao/feegow/tests .
