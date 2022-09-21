1312707ddf8a642a508e3eff589484ffa62a1c1c
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <phtfao@gmail.com>
2022-09-21 09:11:08 -0300

    Adiciona chave do aplicativo.

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!
 
 cp .env.example .env
+composer config --global --auth github-oauth.github.com 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
 composer install
 chmod 777 storage -R
 
 cp -R vendor/phtfao/feegow/tests .
-php /panako/artisan migrate:refresh --seed
+php /feegow/artisan migrate:refresh --seed
